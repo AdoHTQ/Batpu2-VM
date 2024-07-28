d4f0e5654e87796ab7637bebd35bc9a2e93f08c0
Trynera <tawilmustafa223@gmail.com>
2024-07-29 00:50:11 +0200

    Breakpoints :DDD

--- scripts/Assembler.cs
+++ scripts/Assembler.cs
@@ -7,28 +7,28 @@ using Godot;
 
 class Assembler
 {
-    public static string Assemble(string assemblyFilename, string outputFilename)
-    {
-        string pythonPath = "python";
-        string scriptPath = @"assembler/main.py";
-        string[] scriptArgs = {assemblyFilename, outputFilename};
+	public static string Assemble(string assemblyFilename, string outputFilename)
+	{
+		string pythonPath = "python";
+		string scriptPath = @"assembler/main.py";
+		string[] scriptArgs = {assemblyFilename, outputFilename};
 
-        ProcessStartInfo start = new ProcessStartInfo
-        {
-            FileName = pythonPath,
-            Arguments = $"{scriptPath} {string.Join(" ", scriptArgs)}",
-            RedirectStandardError = true,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-        };
+		ProcessStartInfo start = new ProcessStartInfo
+		{
+			FileName = pythonPath,
+			Arguments = $"{scriptPath} {string.Join(" ", scriptArgs)}",
+			RedirectStandardError = true,
+			UseShellExecute = false,
+			CreateNoWindow = true,
+		};
 
-        using (Process process = Process.Start(start))
-        {
-            using (System.IO.StreamReader reader = process.StandardError)
-            {
-                string result = reader.ReadToEnd();
-                return result;
-            }
-        }
-    }
+		using (Process process = Process.Start(start))
+		{
+			using (System.IO.StreamReader reader = process.StandardError)
+			{
+				string result = reader.ReadToEnd();
+				return result;
+			}
+		}
+	}
 }
